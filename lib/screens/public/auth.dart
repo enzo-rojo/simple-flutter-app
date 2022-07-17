83513558e382430c490f23e4fbc0adb6ba728ede
enzo-rs <enzorojoall@gmail.com>
2022-07-17 17:06:16 +0200

    update email state

--- lib/screens/public/auth.dart
+++ lib/screens/public/auth.dart
@@ -9,93 +9,97 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   final _formKey = GlobalKey<FormState>();
+  late String _email;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Center(
-          child: SingleChildScrollView(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                RichText(
-                    text: TextSpan(
-                        text: 'Everyone has\n'.toUpperCase(),
-                        style: TextStyle(
-                          fontSize: 30,
-                          color: Colors.black,
-                        ),
-                        children: [
-                      TextSpan(
-                        text: 'knowledge\n'.toUpperCase(),
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontWeight: FontWeight.bold,
+            child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                  text: TextSpan(
+                      text: 'Everyone has\n'.toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.black,
+                      ),
+                      children: [
+                    TextSpan(
+                      text: 'knowledge\n'.toUpperCase(),
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(text: 'to share'.toUpperCase()),
+                  ])),
+              SizedBox(height: 20),
+              Text('It all starts here.',
+                  style: TextStyle(fontStyle: FontStyle.italic)),
+              SizedBox(
+                height: 50.0,
+              ),
+              Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text('Enter your email'),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) => setState(() => _email = value),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          hintText: 'Ex: john.doe@domain.tld',
                         ),
                       ),
-                      TextSpan(text: 'to share'.toUpperCase()),
-                    ])),
-                SizedBox(height: 20),
-                Text('It all starts here.',
-                    style: TextStyle(fontStyle: FontStyle.italic)),
-                SizedBox(
-                  height: 50.0,
-                ),
-                Form(
-                    key: _formKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text('Enter your email'),
-                        SizedBox(
-                          height: 10.0,
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(10.0),
                         ),
-                        TextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              borderSide: BorderSide(
-                                color: Colors.grey,
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              borderSide: BorderSide(
-                                color: Colors.grey,
-                              ),
-                            ),
-                            hintText: 'Ex: john.doe@domain.tld',
-                          ),
+                        onPressed: (() {
+                          if (_formKey.currentState!.validate()) {
+                            // If the form is valid, we want to show the next screen
+                            print(_email);
+                          }
+                        }),
+                        child: Text(
+                          'Suivant',
+                          style: TextStyle(fontSize: 20.0),
                         ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                          ),
-                          onPressed: (() {
-                            if (_formKey.currentState!.validate()) {
-                              // If the form is valid, we want to show the next screen
-                              print('send');
-                            }
-                          }),
-                          child: Text('Suivant', style: TextStyle(fontSize: 20.0),),
-                        )
-                      ],
-                    ))
-              ],
-            ),
-          )
-        ),
+                      )
+                    ],
+                  ))
+            ],
+          ),
+        )),
       ),
     );
   }
